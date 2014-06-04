4ae49730485b42d14b27e71f3508158c902a037e
SizBlast <christofvc@gmail.com>
2014-06-04 15:05:06 +0200

    GUI aangepast

---
+++ EventCalendar/web/js/script.js
@@ -0,0 +1,11 @@
+onload = function(){
+    $('#sandbox-container input').datepicker({
+            format: "dd/mm/yyyy",
+            todayBtn: "linked",
+            language: "nl-BE",
+            orientation: "auto",
+            autoclose: true
+    });
+};
+
+
